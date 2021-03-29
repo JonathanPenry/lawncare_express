085e7c575a0c97819d6221adec1f3f2c9b1da008
Jonathan Penry <jonathan.penry@gmail.com>
2021-03-28 19:56:45 -0500

    Adds build lines to app.js to prepare for test deployment.

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const express = require("express");                         // Use express modul
 const bodyParser = require("body-parser");                  // Allows for the body of the req/res to be read
 const app = express();                                      // Creates an object of the express module                                
 const PORT = process.env.PORT || 3000;                      // Defines localhost port as 3000 or port in .env (8080)
-const quotesRoutes = require("/Routes/quotes.routes");      // Connects quotes routes
-const adminRoutes = require("/Routes/admin.routes");        // Connects admin routes
+const quotesRoutes = require("./Routes/quotes.routes");     // Connects quotes routes
+const adminRoutes = require("./Routes/admin.routes");       // Connects admin routes
 
 /////////////
 // APP.USE //
 /////////////
 // app.use mostly sets up middleware (bodyparcer is technically middleware)
-// app.use(express.static(__dirname + "/build"));           // Build (deployment) needs to be at the top before app.use
+app.use(express.static(__dirname + "/build"));              // Build (deployment) needs to be at the top before app.use
 // app.use(session({ secret: process.env.SECRET_KEY }));    // For connection to the database
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use("/admin", adminRoutes);
@@ -38,7 +38,7 @@ res.send('Hello World!?');
     // Basically is it intended for binding the middleware to your application.
     // Path: It is the path for which the middleware function is being called.
     // Callback: Can be a middleware function or series/array of middleware functions.
-// app.get("*", (req, res) => {res.sendFile("/build/index.html", {root:__dirname + "/"})});
+app.get("*", (req, res) => {res.sendFile("/build/index.html", {root:__dirname + "/"})});
 
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
